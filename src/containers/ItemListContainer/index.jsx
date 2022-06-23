9df1aa4e0d8608e2aa6d5dd7a1dcb3c37c1bf826
CordobaFran <francocordobafs@gmail.com>
2022-06-23 11:54:07 -0300

    landing page

--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import PetBanner from '../../assets/img/BannerPetshop.jpg'
+import "./style.css"
 
 const ItemListContainer = ({greeting}) => {
   return (
-    <div>
-        <h1 className="h3 text-center mt-4">{greeting}</h1>
-        <img className="img-fluid"src={PetBanner} alt="banner" />
+    <div className='d-flex justify-content-center'>
+        <h1 className="h3 text-center mt-4 title">{greeting}</h1>
+        <img className="img-fluid banner"src={PetBanner} alt="banner" />
     </div>
   )
 }
