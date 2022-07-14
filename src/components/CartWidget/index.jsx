e3a560f18b2fa9bacef7279a13f3e452964b1e22
CordobaFran <francocordobafs@gmail.com>
2022-07-14 12:24:14 -0300

    context

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { useContext } from 'react';
 import { GrCart } from 'react-icons/gr';
+import { useNavigate } from 'react-router-dom';
 import { Shop } from '../../Context/ShopContext';
 import "./style.css"
 
 const CartWidget = () => {
+  const navigate = useNavigate();
+
   const {cart} = useContext(Shop)
+
+  const toCart = () =>{
+    navigate("/cart")
+  }
+  
   return (
-      <div>
-          <GrCart size="32" className="ml-5"/>
+      <div onClick={toCart}>
+          <GrCart size="32" className="cart ml-5"/>
           {cart.length && <span className='cart__qty'>({cart.length})</span>}
       </div> 
   )
