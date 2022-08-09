ab122755889c72a58529390c25e910d393231b96
CordobaFran <francocordobafs@gmail.com>
2022-08-09 19:30:19 -0300

    added tittles

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -14,7 +14,8 @@ const Item = ({ product }) => {
     <div className='card my-4 itemCard' onClick={handleDetail}>
       <img src={product.image} className='card-img-top itemCard__img' alt="cardimg" />
       <div className='card-body d-flex flex-column'>
-        <h5 className='card-title itemCard__title'>$ {product.price}</h5>
+        <h1 className='card-title text-center h3'>{product.title}</h1>
+        <h5 className='card-title itemCard__price font-weigth-bold h3'>$ {product.price}</h5>
         <button className='btn btn-info justify-item-center w-50 mx-auto'>Comprar</button>
       </div>
     </div>
